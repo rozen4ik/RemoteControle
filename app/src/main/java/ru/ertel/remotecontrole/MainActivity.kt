f2a7fe4c615442b06d4cb578af72ecee90077241
Mikhail Rozenberg <ro3enn@yandex.ru>
2023-01-10 14:30:34 +0300

    Добавление анимация открытия и закрытия шлагбаума по нажатию на него

--- app/src/main/java/ru/ertel/remotecontrole/MainActivity.kt
+++ app/src/main/java/ru/ertel/remotecontrole/MainActivity.kt
@@ -2,10 +2,38 @@ package ru.ertel.remotecontrole
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        imageView.setImageResource(R.drawable.shlakclose)
+
+        imageView.setOnClickListener {
+            if (imageView.tag == "close") {
+                imageView.tag = "open"
+                Handler().postDelayed(Runnable {
+                    imageView.setImageResource(R.drawable.shlakmiddle)
+                    Handler().postDelayed(Runnable {
+                        imageView.setImageResource(R.drawable.shlakopen)
+                    }, 300)
+                }, 300)
+            } else {
+                imageView.tag = "close"
+                Handler().postDelayed(Runnable {
+                    imageView.setImageResource(R.drawable.shlakmiddle)
+                    Handler().postDelayed(Runnable {
+                        imageView.setImageResource(R.drawable.shlakclose)
+                    }, 300)
+                }, 300)
+            }
+        }
     }
 }
